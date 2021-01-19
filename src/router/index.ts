313e1d5f90a616c4631a688d9d8d31813176f3b4
kyleliao0321 <kyleliao032@gmail.com>
2021-01-20 02:54:41 +0800

    Add redirect login for loggin

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
+import store from '../store'
+import VueRouter, { Route, RouteConfig } from 'vue-router'
 import Feeds from '../views/Feeds/Feeds.vue'
 import Home from '../views/Home/Home.vue'
 import Register from '../views/Register/Register.vue'
@@ -11,12 +12,18 @@ const routes: Array<RouteConfig> = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      requiredAuth: true
+    }
   },
   {
     path: '/feeds',
     name: 'Feeds',
-    component: Feeds
+    component: Feeds,
+    meta: {
+      requiredAuth: true
+    }
   },
   {
     path: '/register',
@@ -34,6 +41,20 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+// setup interceptor for authentication checking
+// if given client is not logged in yet, redirect to login page
+router.beforeEach((to: Route, from: Route, next) => {
+  if (to.matched.some(record => record.meta.requiredAuth)) {
+    if (!store.getters.isLogin) {
+      next({ name: 'Login' })
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
