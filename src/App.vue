d9439dea7a157add8128a3eabc519dea7b081943
kyleliao0321 <kyleliao032@gmail.com>
2021-01-20 00:22:47 +0800

    setup router

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,19 @@
       <v-toolbar-title>Demo Application</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn icon>
-        <ig-icons icon="HomeClicked" style="width: 40px; height: 40px;"/>
+        <ig-icons
+          @click.native="navigateToFeeds"
+          :icon="homeIcon"
+          style="width: 40px; height: 40px;"/>
       </v-btn>
-      <ig-avatar :imageSrc="loginUserImageSrc" size="52" />
+      <ig-avatar
+        @click.native="navigateToHome"
+        style="cursor: pointer; "
+        :imageSrc="loginUserImageSrc"
+        size="52" />
     </v-app-bar>
     <v-main style="padding-top: 75px;">
-      <!-- <ig-feeds /> -->
-      <ig-home/>
+      <router-view></router-view>
     </v-main>
   </v-app>
 </div>
@@ -22,16 +28,31 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import Feed from './views/Feeds/Feeds.vue'
-import Home from './views/Home/Home.vue'
-
-Vue.component('ig-feeds', Feed)
-Vue.component('ig-home', Home)
 
 @Component
 export default class App extends Vue {
   get loginUserImageSrc (): string {
     return 'https://cdn.vuetifyjs.com/images/john.jpg'
+  }
+
+  get currentRouteName (): string {
+    return this.$route.name ?? ''
+  }
+
+  get homeIcon (): string {
+    return this.currentRouteName === 'Feeds' ? 'HomeClicked' : 'Home'
+  }
+
+  private navigateToFeeds () {
+    if (this.currentRouteName !== 'Feeds') {
+      this.$router.push({ name: 'Feeds' })
+    }
+  }
+
+  private navigateToHome () {
+    if (this.currentRouteName !== 'Home') {
+      this.$router.push({ name: 'Home' })
+    }
   }
 }
 </script>
